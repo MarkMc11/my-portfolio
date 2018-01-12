0fd36cf35b578808fccb847ee632edccd74ca0ee
Mark McQuade <mmcquade@unityconnected.com>
2018-01-12 10:32:45 -0500

    Moved static markup from html to react

--- js/main.js
+++ js/main.js
@@ -1,5 +1,48 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
+const myWork = [
+  {
+  'title': "AWS Certified Developer Associate",
+  'image': {
+    'desc': "example screenshot of a project involving code",
+    'src': "images/example1.png",
+    'comment': ""
+    }
+  },
+  {
+  'title': "AWS Certified SysOps Administrator Associate",
+  'image': {
+    'desc': "example screenshot of a project involving code",
+    'src': "images/example1.png",
+    'comment': ""
+    }
+  },
+  {
+  'title': "AWS Certified Solutions Architect Associate",
+  'image': {
+    'desc': "example screenshot of a project involving code",
+    'src': "images/example1.png",
+    'comment': ""
+    }
+  },
+  {
+  'title': "AWS Certified Solutions Architect Professional",
+  'image': {
+    'desc': "example screenshot of a project involving code",
+    'src': "images/example1.png",
+    'comment': ""
+    }
+  },
+  {
+  'title': "AWS Certified DevOps Professional",
+  'image': {
+    'desc': "example screenshot of a project involving code",
+    'src': "images/example1.png",
+    'comment': ""
+    }
+  }
+]
 
-console.log("Webpack works");
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
